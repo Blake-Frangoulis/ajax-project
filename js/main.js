e77fbe85c2e7de516a6543dda8d7e6cc5baa729d
Blake Frangoulis <blakef44@gmail.com>
2023-06-23 05:43:07 +0000

    created renderTopAnime and renderTopManga functions to render a DOM tree for an entry in the topAnime or topManga array and prepare it to be appended when function is called. added event listeners to anime and manga anchors on navbar to utilize the two functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+const $topAnimeList = document.querySelector('.top-anime-list');
+const $topMangaList = document.querySelector('.top-manga-list');
+const $animeAnchor = document.querySelector('.anime-anchor');
+const $mangaAnchor = document.querySelector('manga-anchor');
 const topAnime = [];
-// const topManga = [];
+const topManga = [];
 
 function getTopAnime() {
   const xhr = new XMLHttpRequest();
@@ -14,18 +18,105 @@ function getTopAnime() {
 }
 
 getTopAnime();
-// console.log('topAnime:', topAnime);
 
-// function getTopManga() {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'https://api.jikan.moe/v4/top/manga');
-//   xhr.responseType = 'json';
-//   xhr.addEventListener('load', function () {
-//     for (let i = 0; i <= 24; i++) {
-//       topManga.push(xhr.response.data[i]);
-//     }
-//   });
-//   xhr.send();
-// }
+function getTopManga() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.jikan.moe/v4/top/manga');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    for (let i = 0; i <= 24; i++) {
+      topManga.push(xhr.response.data[i]);
+    }
+  });
+  xhr.send();
+}
 
-// getTopManga();
+getTopManga();
+
+function renderTopAnime(card) {
+
+  const $cardRendered = document.createElement('div');
+  $cardRendered.classList.add('column-fifth');
+
+  const $card = document.createElement('div');
+  $card.classList.add('card');
+  $cardRendered.appendChild($card);
+
+  const $cardHeader = document.createElement('div');
+  $cardHeader.classList.add('card-header', 'row');
+  $card.appendChild($cardHeader);
+
+  const $cardRank = document.createElement('h3');
+  $cardRank.textContent = '#' + topAnime[card].rank;
+  $cardHeader.appendChild($cardRank);
+
+  const $imageHolder = document.createElement('div');
+  $imageHolder.classList.add('row', 'image-holder');
+  $card.appendChild($imageHolder);
+
+  const $image = document.createElement('img');
+  $image.setAttribute('src', topAnime[card].images.jpg.image_url);
+  $image.setAttribute('alt', `${topAnime[card].title}-image`);
+  $imageHolder.appendChild($image);
+
+  const $cardTitle = document.createElement('div');
+  $cardTitle.classList.add('row', 'card-title');
+  $card.appendChild($cardTitle);
+
+  const $title = document.createElement('p');
+  $title.textContent = topAnime[card].title;
+  $cardTitle.appendChild($title);
+
+  return $cardRendered;
+}
+
+function renderTopManga(card) {
+
+  const $cardRendered = document.createElement('div');
+  $cardRendered.classList.add('column-fifth');
+
+  const $card = document.createElement('div');
+  $card.classList.add('card');
+  $cardRendered.appendChild($card);
+
+  const $cardHeader = document.createElement('div');
+  $cardHeader.classList.add('card-header', 'row');
+  $card.appendChild($cardHeader);
+
+  const $cardRank = document.createElement('h3');
+  $cardRank.textContent = '#' + topManga[card].rank;
+  $cardHeader.appendChild($cardRank);
+
+  const $imageHolder = document.createElement('div');
+  $imageHolder.classList.add('row', 'image-holder');
+  $card.appendChild($imageHolder);
+
+  const $image = document.createElement('img');
+  $image.setAttribute('src', topManga[card].images.jpg.image_url);
+  $image.setAttribute('alt', `${topManga[card].title}-image`);
+  $imageHolder.appendChild($image);
+
+  const $cardTitle = document.createElement('div');
+  $cardTitle.classList.add('row', 'card-title');
+  $card.appendChild($cardTitle);
+
+  const $title = document.createElement('p');
+  $title.textContent = topManga[card].title;
+  $cardTitle.appendChild($title);
+
+  return $cardRendered;
+}
+
+function createTopAnimeList(event) {
+  for (let card = 0; card <= 24; card++) {
+    $topAnimeList.appendChild(renderTopAnime(card));
+  }
+}
+$animeAnchor.addEventListener('click', createTopAnimeList);
+
+function createTopMangaList(event) {
+  for (let card = 0; card <= 24; card++) {
+    $topMangaList.appendChild(renderTopManga(card));
+  }
+}
+$mangaAnchor.addEventListener('click', createTopMangaList);
